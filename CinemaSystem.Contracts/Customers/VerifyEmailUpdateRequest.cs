25ba1fb6f953716de2ec6d7e22fd2ad173f2660d
Võ Thái Hoàng Khôi <yu9696000@gmail.com>
2026-07-16 16:07:33 +0700

    caideoma

--- CinemaSystem.Contracts/Customers/VerifyEmailUpdateRequest.cs
+++ CinemaSystem.Contracts/Customers/VerifyEmailUpdateRequest.cs
@@ -1,8 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace CinemaSystem.Contracts.Customers;
 
 public sealed class VerifyEmailUpdateRequest
 {
+    [Required]
+    [EmailAddress]
+    [MaxLength(255)]
     public string NewEmail { get; init; } = string.Empty;
+
+    [Required]
+    [MaxLength(10)]
     public string Otp { get; init; } = string.Empty;
+
+    [Required]
+    [MaxLength(10)]
     public string OldEmailOtp { get; init; } = string.Empty;
 }
